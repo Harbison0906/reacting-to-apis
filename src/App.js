983c79ffa95a144db6f40ab8cc8cf3ff61a1ae69
Seth Harbison <sethharbison@Seths-MacBook-Pro.local>
2020-04-01 22:37:57 -0500

    created fetch request to retrieve films from API and render them as individual cards

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import 'isomorphic-fetch';
+import 'es6-promise';
+// import { directive } from '@babel/types';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      films: []
+    }
+  }
+
+  componentDidMount() {
+    fetch("https://ghibliapi.herokuapp.com/films")
+      .then(res => res.json())
+      .then(movie => {
+        this.setState({ films: movie });
+        console.log(movie.title)
+      });
+  }
+
+  render() {
+    return (
+      <main>
+        {this.state.films.map(movie => {
+          return (
+            <div className="col-md-7">
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">{movie.title}</h5>
+                  <p className="card-text">{movie.description}</p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </main>
+
+
+    )
+  }
+
 }
 
 export default App;
